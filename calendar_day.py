ac428463b1101855c65903c36b533629f4a4bf87
Osama Muhammad <59265090+osamamuhammad3623@users.noreply.github.com>
2020-08-08 16:29:32 +0200

    Add files via upload

---
+++ calendar_day.py
@@ -0,0 +1,10 @@
+import calendar
+
+date = input('Enter the date (dd/mm/yy): ')
+day = int(date.split()[0])
+month = int(date.split()[1])
+year = int(date.split()[2])
+
+day_index = calendar.weekday(year, month, day)
+day_name = calendar.day_name[day_index]
+print (date, 'was',day_name)
